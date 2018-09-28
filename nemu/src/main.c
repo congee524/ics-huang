26f83659f90690ddc84d5b7d755edf5fb0fc560d
tracer-ics2018 <tracer@njuics.org>
2018-09-28 16:21:16 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  16:21:15 up  7:44,  4 users,  load average: 0.09, 0.03, 0.01 9f9fdf3b585843cea29669d9eb9654a2c9bf2c5f

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,22 +7,21 @@ int main(int argc, char *argv[]) {
     /* Initialize the monitor. */
     int is_batch_mode = init_monitor(argc, argv);
 
-    /* test the expr.c */
-    char str_tmp[65530];
+    /* test the expr.c: firstly i use fgets, but the line feeds will be read and it cannot be recognized, so i use fscanf */
+
     FILE *fp;
     if ((fp = fopen("/home/congee/ics2018/nemu/tools/gen-expr/input", "r")) == NULL) {
         printf("open file failed!\n");
         return -1;
     }
-    
-    while (!feof(fp)) {
-        uint32_t res;
-        fscanf(fp, "%u", &res);
-        fgets(str_tmp, 65530, fp);
-        bool success = true;
-        uint32_t ans;
+    uint32_t res, ans;
+    bool success = true;
+    while (~fscanf(fp, "%u", &res)) {
+        char str_tmp[65530];
+        fscanf(fp, "%[^\n]%*c", str_tmp);
+        success = true;
         ans = expr(str_tmp, &success);
-        printf("the true answer is %u. the ans of expr is %u.\n", res, ans);
+        printf("the true answer is %u. the answer of expr is %u.\n", res, ans);
     }
     fclose(fp);
     /* Receive commands from user. */
