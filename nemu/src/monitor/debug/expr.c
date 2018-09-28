e5da7c6c6fdcb70b5b291ebc878a8621547a19c5
tracer-ics2018 <tracer@njuics.org>
2018-09-28 16:24:46 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  16:24:46 up  7:48,  4 users,  load average: 0.00, 0.02, 0.00 8701820db25624401133f46bd6c3a8e1e0c0bb89

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -79,9 +79,11 @@ static bool make_token(char *e) {
                 char *substr_start = e + position;
                 int substr_len = pmatch.rm_eo;
 
+                /*
                 Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
                         i, rules[i].regex, position, substr_len, substr_len, substr_start);
                 position += substr_len;
+                */
 
                 /* TODO: Now a new token is recognized with rules[i]. Add codes
                  * to record the token in the array `tokens'. For certain types
@@ -187,29 +189,29 @@ uint32_t eval(int p, int q) {
                 op_type = '*';
                 op_posi = i;
                 op_prio = 3;            
-                printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
+                // printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
             } else if (tokens[i].type == '/' && op_prio >= 3) {
                 op_type = '/';
                 op_posi = i;
                 op_prio = 3;
-                printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
+                // printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
             } else if (tokens[i].type == '+' && op_prio >= 2) {
                 op_type = '+';
                 op_posi = i;
                 op_prio = 2;
-                printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
+                // printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
             } else if (tokens[i].type == '-' && op_prio >= 2) {
                 op_type = '-';
                 op_posi = i;
                 op_prio = 2;
-                printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
+                // printf("p = %d, q = %d, i = %d, op_type = %d\n ", p, q, i, op_type);
             }    
         }
         uint32_t val1 = 0, val2 = 0;
         // printf("%d~%d, op_posi: %d\n", p, q, op_posi);
         val1 = eval(p, op_posi - 1);
         val2 = eval(op_posi + 1, q);
-        printf("val1 = %u, val2 = %u\n", val1, val2);
+        // printf("val1 = %u, val2 = %u\n", val1, val2);
         switch (op_type) {
             case '*':
                 return val1 * val2;
