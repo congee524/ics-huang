2cf486133b12addc661e610f462f716dd36d9e96
tracer-ics2018 <tracer@njuics.org>
2018-09-29 17:39:17 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  17:39:17 up 22:29,  4 users,  load average: 0.26, 0.17, 0.06 46ed1f73302dde5744c0f544dcb28b002b3ce57a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -131,7 +131,7 @@ static bool make_token(char *e) {
                     case REG:
                         empty_token_str();
                         strncpy(tokens[nr_token].str, substr_start + 1, substr_len);
-                        tokens[nr_token].str[substr_len + 1] = '\0';
+                        tokens[nr_token].str[substr_len] = '\0';
                         tokens[nr_token].type = rules[i].token_type;
                         break;
                     case '+':
