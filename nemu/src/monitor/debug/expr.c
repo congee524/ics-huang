9ce960942e4f3f9f1b2dce8d4e9e6578a421081d
tracer-ics2018 <tracer@njuics.org>
2018-09-29 05:26:27 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  05:26:27 up 13:42,  4 users,  load average: 0.07, 0.03, 0.00 e67d2cee42a8de1224c2e2a1faa6c5257bab33ae

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -223,7 +223,7 @@ uint32_t eval(int p, int q) {
             case '+':
                 return val1 + val2;
             case '-':
-                return val1 - val2;
+                return (uint32_t)(val1 - val2);
             default:
                 printf("wrong op_type!\n");
                 assert(0);
