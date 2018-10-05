e5dbf91e64fdf6630096ef3d068c6bbef45113e0
tracer-ics2018 <tracer@njuics.org>
2018-10-05 14:03:03 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  14:03:03 up 20:57,  5 users,  load average: 0.01, 0.05, 0.02 c0506cbde33ea50d2eea56d21923e76d4f1a8d9e

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -32,7 +32,9 @@ make_EHelper(call) {
 }
 
 make_EHelper(ret) {
-  rtl_pop(&cpu.eip);
+  // rtl_pop(&decoding.seq_eip);
+  rtl_pop(&decoding.jmp_eip);
+  rtl_j(decoding.jmp_eip);
   print_asm("ret");
 }
 
