443e9c949aded03edb7bd74f35ed3d7c00cc5dd9
tracer-ics2018 <tracer@njuics.org>
2018-10-05 07:56:46 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  07:56:46 up 14:51, 17 users,  load average: 0.01, 0.02, 0.00 781c393b6df520c452c1779f7ddf5a816edd271a

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -204,7 +204,11 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+    if ((*result << (32 - width * 8)) == 0) {
+        cpu.eflags.ZF = 0;
+    } else {
+        cpu.eflags.ZF = 1;
+    }
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
