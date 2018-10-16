ca9b5f1b489a79ac8d03b0d1adc7fc4cadb02a6d
tracer-ics2018 <tracer@njuics.org>
2018-10-16 16:26:09 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  16:26:09 up 22:14,  7 users,  load average: 0.09, 0.02, 0.01 b377251dc73e190938e1db6ac90e302a7ab5a280

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -165,7 +165,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp -= 4;
-  rtl_sm(&reg_l(4), src1, 4);
+  rtl_sm(&reg_l(5), src1, 4);
 }
 
   
