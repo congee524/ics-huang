386a6b72b75f90bc138b58b5a3c24b4a91457902
tracer-ics2018 <tracer@njuics.org>
2018-10-17 16:37:15 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  16:37:15 up 1 day,  9:36, 17 users,  load average: 0.40, 0.20, 0.07 71a92d5cee0c64d74a2a3e1d3961f03b9c770dfd

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -25,6 +25,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
         //}
         //printf("zf is %u\n", *dest);
         break;
+    case CC_NE:
+        *dest = !cpu.eflags.ZF;
     case CC_BE:
     case CC_S:
     case CC_L:
