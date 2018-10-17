1f29a8e5c7d7d94f41159c9617d18821b8e73a12
tracer-ics2018 <tracer@njuics.org>
2018-10-17 11:27:52 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  11:27:52 up 1 day,  4:38, 16 users,  load average: 0.40, 0.08, 0.03 8c07a98d4eb90781d81c9f0dfce41d2c7383560d

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -46,6 +46,7 @@ static inline make_DopHelper(SI) {
   if (op->width == 1) {
       op->simm = (op->simm << 24) >> 24;
   }
+  printf("op->val is 0x%x\n", op->val);
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
@@ -210,6 +211,7 @@ make_DHelper(SI2E) {
   assert(id_dest->width == 2 || id_dest->width == 4);
   decode_op_rm(eip, id_dest, true, NULL, false);
   id_src->width = 1;
+  printf("id_src is 0x%x\n", id_src->val);
   decode_op_SI(eip, id_src, true);
   if (id_dest->width == 2) {
     id_src->val &= 0xffff;
