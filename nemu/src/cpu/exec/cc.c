0296d123080076dd7e226425b3b755be1d30efd0
tracer-ics2018 <tracer@njuics.org>
2018-10-17 17:05:54 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  17:05:54 up 1 day, 10:04, 17 users,  load average: 0.16, 0.05, 0.01 257249db423d12be283bfaff355589d51152c55

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -13,9 +13,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
-  int tmp = subcode & 0xe;
-  printf("#%d\n", tmp);
-  switch (subcode & 0xe) {
+  //switch (subcode & 0xe) {
+  switch(subcode) {
     case CC_O:
     case CC_B:
     case CC_E:
@@ -32,6 +31,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
         if (cpu.eflags.ZF == 0) {*dest = 1;} else {*dest = 0;}
         //*dest = !cpu.eflags.ZF;
         printf("#NE %u\n", *dest);
+        break;
     case CC_BE:
     case CC_S:
     case CC_L:
