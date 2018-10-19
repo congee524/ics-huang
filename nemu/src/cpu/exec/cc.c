e1fddf9366f0f57f709e27a7c8902cdb1fa88aa8
tracer-ics2018 <tracer@njuics.org>
2018-10-19 05:28:44 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  05:28:44 up  7:10, 12 users,  load average: 0.09, 0.09, 0.02 e7c86e42402949c7c7a79e61fb89434bdbfceb05

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,29 +15,30 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+        *dest = cpu.eflags.OF;
+        break;
     case CC_B:
+        *dest = cpu.eflags.CF;
+        break;
     case CC_E:
-        //printf("no!!!\n");
         *dest = cpu.eflags.ZF;
-        //if (cpu.eflags.ZF == 1) {
-        //    *dest = 1;
-        //} else {
-        //    *dest = 0;
-        //}
-        //printf("zf is %u\n", *dest);
         break;
     case CC_NE:
-        //if (cpu.eflags.ZF == 0) {*dest = 1;} else {*dest = 0;}
         *dest = !cpu.eflags.ZF;
-        //printf("#NE %u\n", *dest);
         break;
     case CC_BE:
+        *dest = cpu.eflags.CF | cpu.eflags.ZF;
+        break;
     case CC_S:
+        *dest = cpu.eflags.SF;
+        break;
     case CC_L:
         *dest = (cpu.eflags.SF != cpu.eflags.OF ? 1 : 0);
         break;
     case CC_LE:
-      TODO();
+      //TODO();
+        *dest = (!cpu.eflags.ZF) && (cpu.eflags.SF == cpu.eflags.OF);
+        break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
