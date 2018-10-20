d4ee06d8efaba46543ee63ca39256944d6701781
tracer-ics2018 <tracer@njuics.org>
2018-10-20 02:39:57 +0000

    >  compile 171240513 congee Linux 43331bfae034 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  02:39:57 up 16:12, 13 users,  load average: 0.01, 0.02, 0.00 f88d0ead8f3b592a4409d5d434c650154043023f

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -66,11 +66,11 @@ make_EHelper(shl) {
 make_EHelper(shr) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  id_dest->simm = id_dest->val;
+  int32_t tmp_val = id_dest->val;
   while(id_src->val--) {
-      id_dest->simm >>= 1;
+      tmp_val >>= 1;
   }
-  id_dest->val = id_dest->simm;
+  id_dest->val = tmp_val;
   operand_write(id_dest, &id_dest->val);
   
   rtl_update_ZFSF(&id_dest->val, id_dest->width);
