2f1df16e4be7583374d76609e46a20bbf72b8ea2
tracer-ics2018 <tracer@njuics.org>
2018-11-02 15:17:48 +0000

    >  compile 171240513 congee Linux 100a6525a810 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  15:17:48 up 15:12, 25 users,  load average: 0.01, 0.20, 0.29 8bf9dbb555e059f3478e02d88dafce3673f9becb

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -85,10 +85,9 @@ make_EHelper(setcc) {
   //printf("t2 is %u\n", t2);
   //rtl_mv(&at, &reg_l(id_dest->reg));
   printf("after ecx is 0x%x\nid_dest->val is 0x%x\n", cpu.ecx, id_dest->val);
-  //at = (at >> 8) << 8;
-  //rtl_or(&id_dest->val, &at, &t2);
-  //operand_write(id_dest, &id_dest->val);
-  operand_write(id_dest, &t2);
+  id_dest->val = (id_dest->val >> 8) << 8;
+  rtl_or(&id_dest->val, &id_dest->val, &t2);
+  operand_write(id_dest, &id_dest->val);
   print_asm("set%s %s", get_cc_name(cc), id_dest->str);
 }
 
