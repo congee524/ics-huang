7dc0e557a01527fa12313c6651f07a657f3eec77
tracer-ics2018 <tracer@njuics.org>
2018-11-03 18:47:27 +0000

    >  run 171240513 congee Linux 100a6525a810 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  18:47:27 up 1 day,  4:51, 17 users,  load average: 0.00, 0.09, 0.27 ce81ff7c8d2daf4a9caf090a7c72833f6a9e1a0

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -1,13 +1,20 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
+#define I8042_DATA_PORT 0x60
+#define KEYDOWN_MASK 0x8000
 
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      kbd->keycode = inl(I8042_DATA_PORT);
+      if (kbd->keycode & KEYDOWN_MASK) {
+          kbd->keydown = 1;
+      } else {
+          kbd->keydown = _KEY_NONE;
+      }
+      //kbd->keycode = _KEY_NONE;
       return sizeof(_KbdReg);
     }
   }
