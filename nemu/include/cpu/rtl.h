ccfc198a9cc67033c139760546e04c3033e978e6
tracer-ics2018 <tracer@njuics.org>
2018-11-04 13:27:45 +0800

    >  compile 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:27:45 up 33 min, 11 users,  load average: 0.02, 0.11, 0.24 4df3da73dc2072fbb08f06f77bfa525c33d8338

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -78,8 +78,8 @@ static inline void interpret_rtl_idiv64_r(rtlreg_t* dest,
   *dest = dividend % divisor;
 }
 
-static inline void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
-//inline void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
+//static inline void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
+inline void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 //static void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 //void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 *dest = vaddr_read(*addr, len);
