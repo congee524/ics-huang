66a4b95519738621a0b62755dcd73235b43aee21
tracer-ics2018 <tracer@njuics.org>
2018-11-04 00:07:40 +0000

    >  run 171240513 congee Linux 100a6525a810 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  00:07:40 up 1 day,  5:42, 14 users,  load average: 0.03, 0.09, 0.09 fc256f5210ecc3fceddfbcc83ebcde0c519e0ede

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -24,6 +24,7 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
 size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
+      assert(0);
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
