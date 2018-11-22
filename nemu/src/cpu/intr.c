cae36da382214356795656aaf8c0b68de8260e21
tracer-ics2018 <tracer@njuics.org>
2018-11-22 21:25:28 +0800

    >  compile 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:25:28 up  4:29, 12 users,  load average: 0.07, 0.09, 0.06 bf240fc3661c99f0b8380f5ad45547c7bf731954

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -24,9 +24,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
     uint32_t val[2];
   } GD;
   Log("5");
-  GD.val[0] = vaddr_read(cpu.IDTR.base + NO * 8, 4);
+  GD.val[0] = vaddr_read(cpu.IDTR.base + NO, 4);
   Log("6");
-  GD.val[1] = vaddr_read(cpu.IDTR.base + NO * 8 + 4, 4);
+  GD.val[1] = vaddr_read(cpu.IDTR.base + NO + 4, 4);
   Log("15_0 = 0x%x\n31_16 = 0x%x\neip = 0x%x", GD.gate.offset_15_0, GD.gate.offset_31_16, cpu.eip);
   cpu.eip = GD.gate.offset_15_0 + (GD.gate.offset_31_16 << 16);
   Log("after eip = 0x%x", cpu.eip);
