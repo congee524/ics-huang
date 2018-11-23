17a647091d932b01d5f211996b168213a092808f
tracer-ics2018 <tracer@njuics.org>
2018-11-23 08:21:52 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:21:52 up  8:18, 24 users,  load average: 0.16, 0.24, 0.37 34187cab2a52f2a2135c714861595deaa56db488

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,8 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case _EVENT_YIELD: assert(0); break;
+    case _EVENT_SYSCALL: assert(0); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
