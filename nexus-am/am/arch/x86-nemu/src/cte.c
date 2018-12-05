e4b08ace9e14331021af8c1469a6b928eb112cd0
tracer-ics2018 <tracer@njuics.org>
2018-12-05 20:25:09 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:25:09 up 30 min,  8 users,  load average: 0.41, 0.18, 0.25 427427b45eb4cc591bbf176bdc9475db5169e269

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -9,7 +9,7 @@ void vecnull();
 
 _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
-  /* 
+   
   printf("tf 0x%x\n", tf);
   printf("tf->eflags = 0x%x\n", tf->eflags);
   printf("ef->cs = 0x%x\n", tf->cs);
@@ -24,12 +24,11 @@ _Context* irq_handle(_Context *tf) {
   printf("tf->esi = 0x%x\n", tf->esi);
   printf("tf->edi = 0x%x\n", tf->edi);
   printf("tf->eip = 0x%x\n", tf->eip);
-  */
+  
 
   if (user_handler) {
     _Event ev;
     //printf("1");
-    printf("tf->irq: 0x%d\n", tf->irq);
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
       case 0x81: ev.event = _EVENT_YIELD; break;
