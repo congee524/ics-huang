32cf1eda5f5439840d3265cf22c755bfe0b2ed68
tracer-ics2018 <tracer@njuics.org>
2018-12-06 00:32:38 +0800

    >  compile 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:32:38 up  1:39,  8 users,  load average: 0.08, 0.03, 0.01 aef17373452c7942bd8c91c7f77749b8c1eb5fd

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -48,9 +48,11 @@ make_EHelper(int) {
 
 make_EHelper(iret) {
   //TODO();
-  rtl_pop(&decoding.seq_eip);
+  rtl_pop(&decoding.jmp_eip);
+  //rtl_pop(&decoding.seq_eip);
   rtl_pop(&cpu.cs);
   rtl_pop(&cpu.eflags.val);
+  rtl_j(decoding.jmp_eip);
   print_asm("iret");
 }
 
