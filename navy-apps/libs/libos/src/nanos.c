146d260b0458020f028db6e82ec2954d4c63eba8
tracer-ics2018 <tracer@njuics.org>
2018-12-06 01:10:15 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:10:15 up  2:17, 14 users,  load average: 0.04, 0.16, 0.14 260c3875dc17e07713c58102d6172dae6ef9f895

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -9,14 +9,12 @@
 #if defined(__ISA_X86__)
 intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
   int ret = -1;
-  assert(0);
   asm volatile("int $0x80": "=a"(ret): "a"(type), "b"(a0), "c"(a1), "d"(a2));
   return ret;
 }
 #elif defined(__ISA_AM_NATIVE__)
 intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
   intptr_t ret = 0;
-  assert(0);
   asm volatile("call *0x100000": "=a"(ret): "a"(type), "S"(a0), "d"(a1), "c"(a2));
   return ret;
 }
