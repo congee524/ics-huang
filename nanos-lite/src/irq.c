3b052e296eaebfcf47ae56b71f9fcd470d1b13a8
tracer-ics2018 <tracer@njuics.org>
2018-12-05 20:13:24 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:13:24 up 18 min,  6 users,  load average: 0.16, 0.36, 0.41 1b4b7422449e4131eab2a72e8a59ef0ab109270

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 static _Context* do_event(_Event e, _Context* c) {
+  printf("e.event: %d\n", e.event);
   switch (e.event) {
     case _EVENT_YIELD: printf("into EVENT_YIELD\n"); break;
     case _EVENT_SYSCALL: assert(0); break;
