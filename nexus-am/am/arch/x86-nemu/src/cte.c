f8ce811a37aea89afa75365fc0d75fe4c58408e1
tracer-ics2018 <tracer@njuics.org>
2018-12-05 20:18:35 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:18:35 up 23 min,  6 users,  load average: 0.07, 0.15, 0.30 1c72d7ba974ebdd44b7d37a9a1d6307985600335

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -29,6 +29,7 @@ _Context* irq_handle(_Context *tf) {
   if (user_handler) {
     _Event ev;
     //printf("1");
+    printf("tf->irq: 0x%x\n", tf->irq);
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
       case 0x81: ev.event = _EVENT_YIELD; break;
