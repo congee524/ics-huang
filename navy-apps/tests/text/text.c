6a903afe4b9dc67d074cbec8aa8dd85fe419c3f3
tracer-ics2018 <tracer@njuics.org>
2018-12-06 22:17:39 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:17:39 up 14:59, 39 users,  load average: 1.29, 1.13, 0.57 a560f76df4cf71cdb0d7cba38c92dfc37bc0477

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -2,10 +2,10 @@
 #include <assert.h>
 
 int main() {
-  /*
+  
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
-
+  /*
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
