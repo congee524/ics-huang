2185c08fcaa68c5a983a114245f1cbf397a0362c
tracer-ics2018 <tracer@njuics.org>
2018-12-06 13:51:05 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:51:05 up  8:25, 39 users,  load average: 1.40, 1.63, 1.04 9c700221f6a6f42b664626aed84374e19158b7c4

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -61,6 +61,7 @@ int sys_yield() {
 int sys_write(int fd, void *buf, size_t count){
   //Log();
   if (fd == 1 || fd == 2) {
+    assert(fd == 1);
     for (int i = 0; i < count; i++) {
       _putc(((char*)buf)[i]);
     }
