de62032f68fa803961adaec06dd30d08f686d029
tracer-ics2018 <tracer@njuics.org>
2018-12-06 12:49:31 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  12:49:31 up  7:45, 39 users,  load average: 0.06, 0.04, 0.07 2da30dd3802ff8a34415098dc29c3aeee4490469

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,7 @@
 
 int sys_yield();
 int sys_write(int fd, void *buf, size_t count);
+int sys_brk(intptr_t newbrk);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -11,32 +12,30 @@ _Context* do_syscall(_Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   /*
-  printf("a[0]: %p\n", a[0]);
-  printf("a[1]: %p\n", a[1]);
-  printf("a[2]: %p\n", a[2]);
-  printf("a[3]: %p\n", a[3]);
-  */
+     printf("a[0]: %p\n", a[0]);
+     printf("a[1]: %p\n", a[1]);
+     printf("a[2]: %p\n", a[2]);
+     printf("a[3]: %p\n", a[3]);
+     */
   switch (a[0]) {
     // SPEC.md say code should be 0, since return value is put in eax, we may use a[1]
     // _yield() return nothing, therefore, complete a funciton sys_yield
-    case SYS_exit: 
-      _halt(a[1]); 
-      break;
-    case SYS_yield:{ 
-                     c->GPRx = sys_yield(); 
-                     break;
-                   }
+    case SYS_exit:  _halt(a[1]); 
+                    break;
+    case SYS_yield: c->GPRx = sys_yield(); 
+                    break;
+    case SYS_write: c->GPRx = sys_write((int)a[1], (void *)a[2], (size_t)a[3]);
+                    break;
+    case SYS_brk:   c->GPRx = sys_brk((intptr_t)a[1]);
+                    break;
     case SYS_open: assert(0); break;
     case SYS_read: assert(0); break;
-    case SYS_write: {
-                      c->GPRx = sys_write((int)a[1], (void *)a[2], (size_t)a[3]);
-                      break;
-                    }
+
+
     case SYS_kill: assert(0); break;
     case SYS_getpid: assert(0); break;
     case SYS_close: assert(0); break;
     case SYS_lseek: assert(0); break;
-    case SYS_brk: assert(0); break;
     case SYS_fstat: assert(0); break;
     case SYS_time: assert(0); break;
     case SYS_signal: assert(0); break;
@@ -68,4 +67,9 @@ int sys_write(int fd, void *buf, size_t count){
     assert(0);
   }
   return count;
+}
+
+int sys_brk(intptr_t newbrk){
+  // i should judge the range of newbrk, but    return dirctly;
+  return 0;
 }
