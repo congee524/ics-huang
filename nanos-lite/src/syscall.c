c98f605ea73f916d59adfae9d9d32eedb24e3233
tracer-ics2018 <tracer@njuics.org>
2018-12-06 08:40:41 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:40:41 up  3:36, 30 users,  load average: 0.03, 0.05, 0.12 59b55006e1f2eceb766e449b9d3619595ce931f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,8 +14,28 @@ _Context* do_syscall(_Context *c) {
   printf("a[3]: %p\n", a[3]);
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
+    case SYS_exit: assert(0); break;
+    case SYS_yield: _yield(); break;
+    case SYS_open: assert(0); break;
+    case SYS_read: assert(0); break;
+    case SYS_write: assert(0); break;
+    case SYS_kill: assert(0); break;
+    case SYS_getpid: assert(0); break;
+    case SYS_close: assert(0); break;
+    case SYS_lseek: assert(0); break;
+    case SYS_brk: assert(0); break;
+    case SYS_fstat: assert(0); break;
+    case SYS_time: assert(0); break;
+    case SYS_signal: assert(0); break;
+    case SYS_execve: assert(0); break;
+    case SYS_fork: assert(0); break;
+    case SYS_link: assert(0); break;
+    case SYS_unlink: assert(0); break;
+    case SYS_wait: assert(0); break;
+    case SYS_times: assert(0); break;
+    case SYS_gettimeofday: assert(0); break;
+  default: panic("Unhandled syscall ID = %d", a[0]);
+}
 
-  return NULL;
+return NULL;
 }
