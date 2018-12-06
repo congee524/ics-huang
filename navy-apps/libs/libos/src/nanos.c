cf7e38b4522274e0a3e3126f33f7c76847e8667e
tracer-ics2018 <tracer@njuics.org>
2018-12-06 14:10:38 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:10:38 up  8:45, 39 users,  load average: 0.02, 0.10, 0.35 2914aeb895c0d08dfee66cbf53e607cc9e4e5e79

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -41,7 +41,7 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  intptr_t oldbrk = &_end;
+  intptr_t oldbrk = pro_brk;
   intptr_t newbrk = oldbrk + increment;
   if(_syscall_(SYS_brk, newbrk, 0, 0) == 0){
     assert(0);
