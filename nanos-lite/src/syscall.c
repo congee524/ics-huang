3fdd7e62ae62c62f43b1d39ee6f6fbb13ca63b96
tracer-ics2018 <tracer@njuics.org>
2018-12-06 08:32:19 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:32:19 up  3:27, 30 users,  load average: 0.13, 0.17, 0.21 17559ac05d99bbef54ea00746e4c0dc1c28b502f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,15 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   printf("a[0]: %p\n", a[0]);
+  printf("a[1]: %p\n", a[1]);
+  printf("a[2]: %p\n", a[2]);
+  printf("a[3]: %p\n", a[3]);
+
   switch (a[0]) {
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
