2562f26aeaa3ff0852bc548a5a404aff30b5ce5a
tracer-ics2018 <tracer@njuics.org>
2018-12-07 17:33:34 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:33:34 up 1 day, 14 min, 36 users,  load average: 0.29, 0.19, 0.11 75c801ab12d5001b9d3b166a2613c18017cd52a8

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include <amdev.h>
+#include "klib.h"
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   for(int i = 0; i < len; i++){
@@ -23,7 +24,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  memcpy(buf, (void *)dispinfo + offset, len);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
