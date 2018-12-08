efe2f008737b7de9a78bf123b7e27d80bf730fe1
tracer-ics2018 <tracer@njuics.org>
2018-12-09 04:13:25 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  04:13:25 up  1:04,  5 users,  load average: 0.29, 0.27, 0.23 c18ce4dfe857f44e45b61f48d17877bba43e2630

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,105 +1,94 @@
 #include "klib.h"
-
+#include <stddef.h>
 //#ifndef __ISA_NATIVE__
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  const char *start = s;
-
-  while(*s)
-    s++;
-  return s - start;
+	size_t i = 0;
+	while(*(s + i) != '\0'){
+		i ++;
+	}
+  return i;
 }
 
-char *strcpy(char* dst,const char* src) {
-  char *s = dst;
-  
-  while ((*dst++ = *src++) != '\0') 
-      ;
-
-  return s;
+char* strcpy(char* dst,const char* src) {
+	int i = 0;
+	while(*(src + i) != '\0'){
+		*(dst + i) = *(src + i);
+		i ++;
+	}
+	return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  char *dscan;
-  const char *sscan;
-  dscan = dst;
-  sscan = src;
-  while (n > 0) {
-    --n;
-    if ((*dscan++ = *sscan++) == '\0')
-      break;
-  }
-  while (n-- > 0)
-    *dscan++ = '\0';
-  return dst;
+	size_t i;
+	for(i = 0; i < n && src[i] != '\0'; ++i){
+		dst[i] = src[i];
+	}
+	for( ; i < n; ++i){
+		dst[i] = '\0';
+	}
+	return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  char *s = dst;
-
-  while(*dst)
-    dst++;
-
-  while ((*dst++ = *src++) != '\0')
-    ;
-  return s;
+	size_t dst_len = strlen(dst);
+	size_t i = 0;
+	for(i = 0; src[i] != '\0'; ++i){
+		dst[dst_len + i] = src[i];
+	}
+	dst[dst_len + i] = '\0';
+	return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-    while (*s1 != '\0' && *s1 == *s2) {
-        s1++;
-        s2++;
-    }
-  return (*(unsigned char *) s1) - (*(unsigned char *) s2);
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	int len = 0;
+	if(len1 < len2) len = len1;
+	else len = len2;
+	for(int i = 0; i <= len; ++i){
+		if(s1[i] < s2[i]) return -1;
+		else if(s1[i] > s2[i]) return 1;
+	}
+	return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  if (n == 0) 
-    return 0;
-
-  while (n-- != 0 && *s1 == *s2) {
-    if (n == 0 || *s1 == '\0') 
-      break;
-    s1++;
-    s2++;
-  }
-  return (*(unsigned char *) s1) - (*(unsigned char *) s2);
+	for(size_t i = 0; i < n; ++i){
+		if(s1[i] < s2[i]) return -1;
+		else if(s1[i] > s2[i]) return 1;
+	}
+  return 0;
 }
 
-void* memset(void* v, int c, size_t n) {
-  char *s = (char *) v;
-  while (n--){
-    *s++ = (char) c;
-  }
-  return v;
+void* memset(void* s,int c,size_t n) {
+	if(s == NULL || n < 0) return NULL;
+	char* temp = (char*) s;
+	for(int i = 0; i < n; ++i){
+		temp[i] = (char) c;
+	}
+	return s;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
-  char *dst = (char *) out;
-  char *src = (char *) in;
-
-  void *save = out;
-
-  while (n--) {
-    *dst++ = *src++;
-  }
-
-  return save;
+void* memcpy(void* dest, const void* src, size_t n) {
+	if(dest == NULL || n < 0) return NULL;
+	char* chardest = (char*) dest;
+	char* charsrc = (char*) src;
+	for(int i = 0; i < n; ++i){
+		chardest[i] = charsrc[i];
+	}
+	return dest;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  unsigned char *m1 = (unsigned char *) s1;
-  unsigned char *m2 = (unsigned char *) s2;
-
-  while (n--) {
-    if (*m1 != *m2) {
-      return *m1 - *m2;
-    }
-    m1++;
-    m2++;
-  }
-  return 0;
+	char* ch1 = (char*) s1;
+	char* ch2 = (char*) s2;
+	for(int i = 0; i < n; ++i){
+		if(ch1[i] < ch2[i]) return -1;
+		else if(ch1[i] > ch2[i]) return 1;
+	}
+	return 0;
 }
 
 #endif
