42468d40215490201cc9774bdc70115d3338ed97
tracer-ics2018 <tracer@njuics.org>
2018-12-09 03:56:43 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  03:56:43 up 47 min,  6 users,  load average: 0.06, 0.36, 0.30 fad15c0d216c282170f3f35ec0ec41c4f64cff8

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,23 +3,19 @@
 #include <amdev.h>
 #include <klib.h>
 
-#define SCREEN_PORT 0x100
-#define SCREEN_H 300
-#define SCREEN_W 400
-
-int min(int a, int b){
-	if(a <= b) return a;
-	else return b;
-}
+#define min(a, b) (a < b ? a : b)
+#define W 400
+#define H 300
 
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+//static uint32_t fb[W * H];
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = SCREEN_W;
-      info->height = SCREEN_H;
+      info->width = W;
+      info->height = H;
       return sizeof(_VideoInfoReg);
     }
   }
@@ -30,17 +26,17 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-			/*int i;
-			int size = screen_width() * screen_height();
-			for(i = 0; i < size; ++i) fb[i] = i;
-			*/
-			int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-			uint32_t *pixels = ctl->pixels;
-			int cp_bytes = sizeof(uint32_t) * min(w, SCREEN_W - x);
-			for(int j = 0; j < h  && y + j < SCREEN_H; ++j){
-				memcpy(&fb[(y + j) * SCREEN_W + x], pixels, cp_bytes);
-				pixels += w;
-			}
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      for (int j = 0; j < h && y + j < H; j++) {
+          //printf("pixels is 0x%08x\n", *pixels);
+          //fb[(y + j) * W + x] = *pixels;
+          memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+          //printf("the position is %d\n", (y + j) * W + x);
+          //printf("fb is 0x%08x\n", fb[(y + j) * W + x]);
+          pixels += w;
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
@@ -51,4 +47,6 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void vga_init() {
+    memset(fb, 0, W * H * sizeof(uint32_t));
 }
+
