43eb05c0a2529ec47b170612ba5560a2f148420f
tracer-ics2018 <tracer@njuics.org>
2018-12-09 05:44:36 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  05:44:36 up  2:35, 10 users,  load average: 0.02, 0.14, 0.36 62f922b06611bcf6b03e016bfca1bb08c90574d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -86,13 +86,12 @@ ssize_t fs_read(int fd, void *buf, size_t len){
 
 ssize_t fs_write(int fd, const void *buf, size_t len){
   Finfo fo = file_table[fd];
-  int real_len = len;
-  if(fo.open_offset + len >= fo.size){
-      real_len = fo.size - fo.open_offset;
+  if(fd != FD_STDOUT && fd != FD_STDERR && fo.open_offset + len >= fo.size){
+      len = fo.size - fo.open_offset;
     }
    if(file_table[fd].write == NULL) {
-     file_table[fd].open_offset += real_len;
-    return ramdisk_write(buf, fo.disk_offset + fo.open_offset, real_len);
+     file_table[fd].open_offset += len;
+    return ramdisk_write(buf, fo.disk_offset + fo.open_offset, len);
   } else {
     // serial_write | fb_write
     // both of them don't update open_offset
