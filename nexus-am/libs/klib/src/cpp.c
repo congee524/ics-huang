15226acfb1c7b6e09dcc0c8c3522ef642c3c99aa
tracer-ics2018 <tracer@njuics.org>
2018-12-09 04:06:51 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  04:06:51 up 57 min,  5 users,  load average: 0.22, 0.18, 0.20 ed36434b17ed43ec743bbd8888ea6f844862085f

--- nexus-am/libs/klib/src/cpp.c
+++ nexus-am/libs/klib/src/cpp.c
@@ -3,12 +3,18 @@
 #ifndef __ISA_NATIVE__
 
 void __dso_handle() {
+  //TODO();
+  assert(0);
 }
 
 void __cxa_guard_acquire() {
+  //TODO();
+  assert(0);
 }
 
 void __cxa_guard_release() {
+  //TODO();
+  assert(0);
 }
 
 
