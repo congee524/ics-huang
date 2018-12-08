b316c67a81e3b5c19b46c62dc13e0d2f12f42852
tracer-ics2018 <tracer@njuics.org>
2018-12-08 14:18:16 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:18:16 up 1 day,  9:13, 37 users,  load average: 0.09, 0.28, 0.30 2c2c7ac19099560c9a931c0cfbe83721ea2ded9

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -137,7 +137,7 @@ static void get_display_info() {
     sscanf(delim + 1, "%s", value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
-    printf("width %d\n height %d", screen_w, screen_h);
+    printf("width %d\n height %d\n", screen_w, screen_h);
   }
   fclose(dispinfo);
   assert(screen_w > 0 && screen_h > 0);
