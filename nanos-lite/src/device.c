71b50612517c788709dae4ca9c7e72afec8028fe
tracer-ics2018 <tracer@njuics.org>
2018-12-08 13:26:06 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:26:06 up 1 day,  8:21, 37 users,  load average: 0.53, 0.51, 0.27 d70f6c9a8c13d30a95fed6f7f500b28e338ecfce

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,8 +24,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  //assert(0);
-  memcpy(buf, (void *)offset, len);
+  assert(0);
+  memcpy(buf, (void *)dispinfo + offset, len);
   return len;
 }
 
@@ -42,7 +42,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height);
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height());
   Log("the content of dispinfo: %s", dispinfo);
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
