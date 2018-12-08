2e03a2da95cfc144455ad54d3677619d91fe446d
tracer-ics2018 <tracer@njuics.org>
2018-12-09 01:26:56 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:26:56 up  1:45,  6 users,  load average: 0.10, 0.16, 0.16 5837ae9a194175bc14267634d2465610bc3c0822

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -90,11 +90,8 @@ ssize_t fs_read(int fd, void *buf, size_t len){
   } else {
     //return file_table[fd].read(buf, fo.disk_offset + fo.open_offset, len);
     //Log("read: %d", fd);
-    //file_table[fd].open_offset += len;
-    int res = file_table[fd].read(buf, fo.open_offset, len);
-    Log("res %d", res);
-    file_table[fd].open_offset += res;
-    return res;
+    file_table[fd].open_offset += len;
+    return file_table[fd].read(buf, fo.open_offset, len);
   }
 
 }
