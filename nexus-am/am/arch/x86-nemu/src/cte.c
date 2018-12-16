bd84da1d4b7b3186b81bee0f1656bc26dbe2dbad
tracer-ics2018 <tracer@njuics.org>
2018-12-16 22:52:49 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:52:49 up  4:28, 12 users,  load average: 0.24, 0.14, 0.07 b273cb6c174d9a3fbc8255a8b5083d2f839b613a

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -51,9 +51,9 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
 _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
   printf("entry is 0x%x\n", entry);
-  _Context *c = (_Context*)stack.end;
+  _Context *c = (_Context*)stack.end - 1;
   memset(c, 0, sizeof(_Context));
-  c->eip = (uintptr_t)entry;
+  //c->eip = (uintptr_t)entry;
   c->cs = 0x8;
   c->eflags = 0x2;
   //c->ebp = (int)stack.end;
