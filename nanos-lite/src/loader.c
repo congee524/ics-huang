baae938a34e1992c9f1e0637d8574e524b89e646
tracer-ics2018 <tracer@njuics.org>
2018-12-28 00:12:53 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:12:53 up 1 day,  1:53, 37 users,  load average: 0.02, 0.11, 0.08 98cbc841e0533e78e129546e4709b0798794e3a0

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,7 +5,24 @@
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO(); PA3.2: ramdisk_read((void*)DEFAULT_ENTRY, 0, get_ramdisk_size());
   int fd = fs_open(filename, 0, 0);
-  fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));
+  //fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));
+  uint32_t fsize = fs_filesz(fd);
+
+  // int _map(_Protect *p, void *va, void *pa, int prot) {
+  // void* new_page(size_t nr_page) 
+  // pgalloc_usr ???
+  void *va = (void *)DEFAULT_ENTRY;
+  void *pa = NULL;
+  while(fsize > 0) {
+    // nr_page indicate the num of allocated free pages
+    // but if there exist no such many pages, maybe wrong?
+    // so i allocate one page each time
+    pa = new_page(1);
+    _map(&(pcb->as), va, pa, 1);
+    fs_read(fd, pa, PGSIZE);
+    fsize -= PGSIZE;
+    va += PGSIZE;
+  }
   fs_close(fd);
   return DEFAULT_ENTRY;
 }
