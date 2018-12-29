35b28232c564cf68d630bcf6fed88c1e5c52ef7b
tracer-ics2018 <tracer@njuics.org>
2018-12-29 17:34:29 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:34:29 up  2:14, 12 users,  load average: 0.95, 0.44, 0.47 6a178aec3da47ab8c7eb2f68e511a1e1ba8e67a

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -2,11 +2,11 @@
 #include "proc.h"
 
 static void *pf = NULL;
-extern PCB *current;
+
+PCB *current;
 
 void* new_page(size_t nr_page) {
   void *p = pf;
-  // Log("pre %p", pf);
   pf += PGSIZE * nr_page;
   assert(pf < (void *)_heap.end);
   return p;
@@ -16,29 +16,36 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
+
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
-  current->cur_brk = new_brk;
-  if (new_brk - current->max_brk > 0) {
-    Log("1");
-    int nr_page = current->max_brk / PGSIZE + 1;
-    void *va = (void *)(nr_page * PGSIZE);
-    int diff_size = new_brk - (int)va - 1;
-    void *pa = NULL;
-    while (diff_size > 0) {
-      pa = new_page(1);
-      _map(&(current->as), va, pa, 1);
-      diff_size -= PGSIZE;
-      va += PGSIZE;
-    }
-    current->max_brk = new_brk;
-  }
+	//Log("new_brk=%p", new_brk);
+	//Log("max_brk=%p", current->max_brk);
+	//assert(0);
+	current->cur_brk = new_brk;
+	int pre_max_brk = current->max_brk;
+	if(new_brk > current->max_brk) {
+		current->max_brk = new_brk;
+		//Log("now max_brk=%p",current->max_brk);
+		int size_left = new_brk - pre_max_brk;
+		int page_left = PGSIZE  - pre_max_brk%PGSIZE - 1;
+		size_left -= page_left;
+		//Log("_heap.start=%p", _heap.start);
+		void* va = (void*)((pre_max_brk/PGSIZE + 1) * PGSIZE);
+		//Log("start from va=%p", va);
+		//assert(0);
+		while(size_left > 0) {
+			void* pa = new_page(1);
+			_map(&(current->as), va, pa, 1);
+			//Log("map va=%p to pa=%p",va, pa);
+			va += PGSIZE;
+			size_left -= PGSIZE;
+		}
+	}
   return 0;
 }
 
 void init_mm() {
-  // the inital position of page at the start of heap
-  // return the physical address
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
   Log("free physical pages starting from %p", pf);
 
