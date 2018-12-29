2a5b349d0c623b683841b401d7b6270cda27e7b9
tracer-ics2018 <tracer@njuics.org>
2018-12-29 19:36:04 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:36:04 up  4:16, 12 users,  load average: 0.36, 0.21, 0.20 ea598717eb1aa698e460dbc1de3f2d65f354f4e4

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -21,7 +21,7 @@ int mm_brk(uintptr_t new_brk) {
   Log("new_brk 0x%x cur_brk 0x%x max_brk 0x%x", new_brk, current->cur_brk, current->max_brk);
   current->cur_brk = new_brk;
   if (new_brk > current->max_brk) {
-    int nr_page = current->max_brk / PGSIZE + 1;
+    int nr_page = current->max_brk / PGSIZE;
     void *va = (void *)(nr_page * PGSIZE);
     int diff_size = new_brk - (int)va - 1;
     void *pa = NULL;
