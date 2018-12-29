739ca4e3043c1dcb522dbe2913d07fd2e3b027fb
tracer-ics2018 <tracer@njuics.org>
2018-12-29 21:06:23 +0800

    >  run 171240513 congee Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:06:23 up  5:46, 12 users,  load average: 0.74, 0.75, 0.65 5ce2836388ea0550b3c4c98c19d98e9a4bfdae21

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,7 +20,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  _yield();
+  //_yield();
   int scan_code = read_key() & 0xffff;
   if (scan_code) {
     if (scan_code & 0x8000) {
